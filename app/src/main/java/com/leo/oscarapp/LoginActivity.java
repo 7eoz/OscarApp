b53b6c8224e0261a1f8ba2c3db7deddd978d679e
7eoz <thesandrini@gmail.com>
2022-04-24 18:46:37 -0300

    Initial commit

---
+++ app/src/main/java/com/leo/oscarapp/LoginActivity.java
@@ -0,0 +1,54 @@
+package com.leo.oscarapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button loginButton;
+    EditText login, password;
+    TextView forgotYourPassword, register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        loginButton = findViewById(R.id.loginButton);
+        login = findViewById(R.id.loginInput);
+        password = findViewById(R.id.passwordInput);
+        forgotYourPassword = findViewById(R.id.forgotYourPasswordLink);
+        register = findViewById(R.id.registerLink);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        forgotYourPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, ResetPasswordActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
