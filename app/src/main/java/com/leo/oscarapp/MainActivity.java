1be673723d6cbfe17f6a019fde1bf1c0b31d06d4
Filipe de Freitas <filipefreitas@ufpr.br>
2022-04-28 18:12:14 -0300

    Estrutura de menu implementada *botão sair funcional

--- app/src/main/java/com/leo/oscarapp/MainActivity.java
+++ app/src/main/java/com/leo/oscarapp/MainActivity.java
@@ -2,13 +2,25 @@ package com.leo.oscarapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button sairButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sairButton = findViewById(R.id.sairBtn);
+
+        sairButton.setOnClickListener(view -> {
+            finish();
+            System.exit(0);
+        });
+
     }
 }
