b6b17d231592695195bd10ea289e46ae59413d1d
Filipe de Freitas <filipefreitas@ufpr.br>
2022-04-29 20:05:12 -0300

    Menu e ConfirmActivity done
    AINDA É NECESSÁRIO CONECTAR COM BD AS ACTIVITIES: ConfirmActivity e MainActivity

--- app/src/main/java/com/leo/oscarapp/MainActivity.java
+++ app/src/main/java/com/leo/oscarapp/MainActivity.java
@@ -6,20 +6,79 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button sairButton;
+    Button sairButton, filmeButton, diretorButton, votoButton;
+    TextView tokenField;
+    int idFilme, idDiretor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        filmeButton = findViewById(R.id.votarFilme);
+        diretorButton = findViewById(R.id.votarDiretor);
+        votoButton = findViewById(R.id.confirmarVoto);
         sairButton = findViewById(R.id.sairBtn);
+        tokenField = findViewById(R.id.token);
+
+        //---------------------------------------//
+        // SETAR NUMERO DO TOKEN AQUI:
+        tokenField.setText("Token: "+"000");
+        //---------------------------------------//
+
+        filmeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //---------------------------------------//
+                // SETAR ACTIVITY DE FILME AQUI:
+
+                //Intent intent = new Intent(MainActivity.this, <NOME DA ACTIVITY>.class);
+                //startActivity(intent);
+
+                //RETORNAR ID DE FILME PARA VARIAVEL : idFilme
+                //---------------------------------------//
+            }
+        });
+        diretorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //---------------------------------------//
+                // SETAR ACTIVITY DE DIRETOR AQUI:
+
+                //Intent intent = new Intent(MainActivity.this, <NOME DA ACTIVITY>.class);
+                //startActivity(intent);
+
+                //RETORNAR ID DE DIRETOR PARA VARIAVEL : idDiretor
+                //---------------------------------------//
+            }
+        });
+        votoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // VARIAVEIS
+                tokenField.getText(); // Valor do token (String) NÃO PASSAR
+                // idFilme            // Id do filme    (int)
+                // idDiretor          // Id do diretor  (int)
+
+                Intent intent = new Intent(MainActivity.this, ConfirmActivity.class);
+                intent.putExtra("filme",idFilme);
+                intent.putExtra("diretor",idDiretor);
+                startActivity(intent);
+
+
+            }
+        });
 
         sairButton.setOnClickListener(view -> {
-            finish();
-            System.exit(0);
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            intent.putExtra("EXIT",true);
+            startActivity(intent);
         });
 
     }
